f85588a2932ef981f521bd17ff1a5995cd57c41e
huangsy <huangsy1412@gmail.com>
2015-09-06 19:13:35 +0800

    update

--- main.js
+++ main.js
@@ -1,18 +1,55 @@
 (function () {
+
   function $(selector) {
     fn = selector.indexOf('#') > -1 ? 'getElementById' : 'getElementsByClassName'
     return document[fn](selector.slice(1)) || []
   }
 
   chrome.storage.local.get('dingName', function (item) {
-    $('#name').value = item.dingName || '黄诗喻';
+    $('#name').value = item.dingName || 4;
   });
 
-  $('#submit').onclick = function () {
-    chrome.storage.local.set({'dingName': $('#name').value});
-    chrome.tabs.create({
-      url: 'https://jinshuju.net/f/euwlb5',
-    });  
+  function jsonp(url) {
+    var script = document.createElement('script');
+    script.src = url;
+    document.getElementsByTagName('body')[0].appendChild(script);
   }
+
+  function open(val) {
+    chrome.storage.local.set({'list': window.__list__});
+    var val = typeof val === 'number' ? val : $('#name').value;
+    var isNum = !/\D/.test(val);
+    if (isNum) {
+      chrome.storage.local.set({'dingName': val});
+      var timer = setInterval(function () {
+        if (--val <= 0) clearInterval(timer);
+        chrome.tabs.create({
+          url: 'https://jinshuju.net/f/euwlb5'
+        });
+      }, 1000);
+    } else {
+      chrome.storage.local.set({'dingName': val});
+      chrome.tabs.create({
+        url: 'https://jinshuju.net/f/euwlb5'
+      });
+    }
+  }
+
+  window.callback = function (data) {
+    window.__list__ = data;
+  }
+
+  jsonp(chrome.extension.getURL('package.json'));
+  $('#submit').onclick = open;
+  chrome.tabs.onCreated.addListener(function () {
+    var date = new Date();
+    var today = date.toLocaleDateString();
+    var dingDate = localStorage.getItem('dingDate');
+    if (dingDate !== today && date.getDay() >= 0 && date.getDay() < 6 && date.getHours() > 5 && date.getHours() < 12) {
+      open(window.__list__.length);
+      localStorage.setItem('dingDate', today);
+    }
+  });
+  
 })();
 
--- page.js
+++ page.js
@@ -1,13 +1,40 @@
 (function () {
+
+  var ns = 'chromeEx'
+
+  function setItem(key, val) {
+    var storage = JSON.parse(localStorage[ns] || '{}');
+    storage[key] = val;
+    localStorage[ns] = JSON.stringify(storage);
+  }
+
+  function getItem(key) {
+    return JSON.parse(localStorage[ns] || '{}')[key];
+  }
+
   function $(selector) {
     fn = selector.indexOf('#') > -1 ? 'getElementById' : 'getElementsByClassName'
     return document[fn](selector.slice(1)) || []
   }
+
   $('#submission_password').value = '9527';
-  $('.btn-primary').length && $('.btn-primary')[0].click();
+  $('.btn-primary')[0].click();
   chrome.storage.local.get('dingName', function (item) {
-    $('#entry_field_1').value = item.dingName;
-    $('.submit').length && $('.submit')[0].click();
+    var isNum = !/\D/.test(item.dingName);
+    chrome.storage.local.get('list', function (item) {
+      var list = item.list;
+      var date = new Date();
+      if (!isNum) return;
+      var num = getItem('num');
+      num = num + 1;
+      setItem('num', num);
+      document.forms['new_entry']['entry[field_1]'].value = list[num % list.length].name;
+      document.forms['new_entry']['entry[field_9]'].value = 'RJfx';
+      document.forms['new_entry']['entry[field_10]'].value = list[num % list.length].department;
+      var filter = list[num % list.length].filter;
+      if (filter && filter.indexOf(date.getDay()) < 0) return;
+      $('.submit').length && $('.submit')[0].click();
+    });
   })
 })();
 
